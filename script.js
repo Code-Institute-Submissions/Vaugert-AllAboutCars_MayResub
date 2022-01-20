5bdd174e07cf3f660e96147ed9083b683602c3e3
Vaugert <lindstromfabian@gmail.com>
2022-01-20 15:21:32 +0000

    add main css and Java script structure

---
+++ script.js
@@ -0,0 +1,19 @@
+let startButton = document.getElementById('start-btn'); 
+let questionContainer = document.getElementById('question-container') ;
+startButton.addEventListener('click', runGame); 
+
+
+function runGame() {
+    
+    startButton.classList.add('hide')
+    questionContainer.classList.remove('hide')
+
+}
+
+function setNextQuestion() {
+
+}
+
+function checkAnswer () {
+
+}
