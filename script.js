1ddba0181c55bd4a4009e95cd6dcc4dc8521c1bd
Vaugert <lindstromfabian@gmail.com>
2022-01-20 16:52:05 +0000

    added functions to java, for start button and randomize question

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-let startButton = document.getElementById('start-btn'); 
-let questionContainer = document.getElementById('question-container') ;
-startButton.addEventListener('click', runGame); 
+const startButton = document.getElementById('start-btn'); 
+const questionContainer = document.getElementById('question-container') ;
+const questionElement = document.getElementById('question');
+const answerButtons = document.getElementById('answer-btn') ;
 
+let randomQuestion, questionIndex
+
+startButton.addEventListener('click', runGame); 
 
 function runGame() {
     
     startButton.classList.add('hide')
+    randomQuestion = questions.sort(() => Math.random() - 0.5)
+    questionIndex = 0
     questionContainer.classList.remove('hide')
+    setNextQuestion()
+
 
 }
 
 function setNextQuestion() {
+    showQuestion(randomQuestion[questionIndex])
+}
 
+function showQuestion(question) {
+    questionElement.innerText = question.question
 }
 
 function checkAnswer () {
 
 }
+
+const questions = [ {
+    question: 'Which is the fastest car in the World?' ,
+    answers: [
+        {text:'buggati' } ,
+        {text:'lambo'}
+        ]
+
+    }
+]
